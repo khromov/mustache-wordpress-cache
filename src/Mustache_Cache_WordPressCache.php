670f3277e9964d73723d587bed7a5a4a8501af04
Stanislav Khromov <stanislav.khromov@aftonbladet.se>
2015-01-05 19:49:09 +0100

    Fix namespacing issues

--- src/Mustache_Cache_WordPressCache.php
+++ src/Mustache_Cache_WordPressCache.php
@@ -4,7 +4,7 @@ namespace Khromov\Mustache_Cache;
 /**
  * Mustache Cache filesystem implementation using WP Object Cache
  */
-class Mustache_Cache_WordPressCache extends Mustache_Cache_AbstractCache
+class Mustache_Cache_WordPressCache extends \Mustache_Cache_AbstractCache
 {
   const GROUP = 'mustache-cache';
 
@@ -25,13 +25,13 @@ class Mustache_Cache_WordPressCache extends Mustache_Cache_AbstractCache
   {
     if(($value = wp_cache_get($key, self::GROUP)) !== false)
     {
-      $this->log(Mustache_Logger::DEBUG, 'Loaded from WP Object Cache: "{key}"', array('key' => $key));
+      $this->log(\Mustache_Logger::DEBUG, 'Loaded from WP Object Cache: "{key}"', array('key' => $key));
       eval('?>' . $value);
       return true;
     }
     else
     {
-      $this->log(Mustache_Logger::DEBUG, 'Cache miss from WP Object Cache: "{key}"', array('key' => $key));
+      $this->log(\Mustache_Logger::DEBUG, 'Cache miss from WP Object Cache: "{key}"', array('key' => $key));
       return false;
     }
       
@@ -47,7 +47,7 @@ class Mustache_Cache_WordPressCache extends Mustache_Cache_AbstractCache
    */
   public function cache($key, $value)
   {
-    $this->log(Mustache_Logger::DEBUG, 'Adding to WP Object Cache cache: "{key}"', array('key' => $key));
+    $this->log(\Mustache_Logger::DEBUG, 'Adding to WP Object Cache cache: "{key}"', array('key' => $key));
     wp_cache_set($key, $value, self::GROUP);
     eval('?>' . $value);
   }
